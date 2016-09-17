065e82f9a433eb5b95f5b011deb3fdd084c1a03d
Christos Constantinou <snez@snez.org>
2016-09-17 03:29:11 +0100

    Add beforeshadow event with a callback

--- dragula.js
+++ dragula.js
@@ -403,8 +403,10 @@ function dragula (initialContainers, options) {
       reference !== nextEl(item)
     ) {
       _currentSibling = reference;
-      dropTarget.insertBefore(item, reference);
-      drake.emit('shadow', item, dropTarget, _source);
+      drake.emit('beforeshadow', item, dropTarget, _source, function(){
+        dropTarget.insertBefore(item, reference);
+        drake.emit('shadow', item, dropTarget, _source);
+      });
     }
     function moved (type) { drake.emit(type, item, _lastDropTarget, _source); }
     function over () { if (changed) { moved('over'); } }
